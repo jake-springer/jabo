0ef13388e23a4a6d53143c822a393b23cc68a232
LucasJ05 <64053529+LucasHeh47@users.noreply.github.com>
2024-02-24 11:37:37 -0800

    pong move

--- apps/pong.py
+++ apps/pong.py
@@ -8,14 +8,24 @@ from ..lib.input_handler import input_handler
 dimension = Vector2D(128, 64)
 
 player1 = GameObject(Vector2D(0, 0), Vector2D(30, 5))
+speed = 4
 player2 = GameObject(Vector2D(123, 0), Vector2D(30, 5))
 
 game_running = True
+input: input_handler
 
-def start_game(input: input_handler):
+def start_game(_input: input_handler):
+    input = _input
     display = Display()
     while game_running:
+        input.tick()
         player1.display(display)
         player2.display(display)
         
         
+#Important to note increasing Y value is down    
+def move():
+    if(input.up):
+        player1.position.y -= speed
+    if(input.down):
+        player1.position.y += speed
