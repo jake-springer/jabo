6eec23f2e2fe58f84cc7a17b4fde4fda73d25277
LucasJ05 <64053529+LucasHeh47@users.noreply.github.com>
2024-02-24 11:33:02 -0800

    Pong boilerplate
    Mostly input_handler

--- apps/pong.py
+++ apps/pong.py
@@ -3,6 +3,7 @@
 from ..lib._math import Vector2D
 from ..sys.display import Display
 from ..lib.game_lib import GameObject
+from ..lib.input_handler import input_handler
 
 dimension = Vector2D(128, 64)
 
@@ -11,8 +12,10 @@ player2 = GameObject(Vector2D(123, 0), Vector2D(30, 5))
 
 game_running = True
 
-def start_game():
+def start_game(input: input_handler):
     display = Display()
     while game_running:
         player1.display(display)
         player2.display(display)
+        
+        
