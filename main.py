07e6ef5e806b5b2e12a831c24b4a83fcedd065cc
LucasJ05 <darthmonkey062005@gmail.com>
2024-02-23 23:36:55 -0800

    Pixel move shit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from display import Display
 
+dim = (128, 64)
+
 display = Display()
 #x, y, color
-display.pixel()
+for i in range(dim[0]):
+    for j in range(dim[1]):
+        display.pixel(i, j, 1)
+        display.show()
+        display.pixel(i, j, 0)
 
 display.show()
