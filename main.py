2db7cacac4f16d5b9470f44f32e4e8099caf8e58
LucasJ05 <darthmonkey062005@gmail.com>
2024-02-23 23:55:37 -0800

    Bouncy balls

--- main.py
+++ main.py
@@ -13,12 +13,20 @@ while (True):
     position[0] += speed[0]
     position[1] += speed[1]
 
-    if(position[0] > 128 or position[0] < 1):
+    if(position[0] > 128):
         position[0] = 128
         speed[0] = -randint(speed[0]-1, speed[0]+1)
         
+    if(position[0] < 0):
+        position[0] = 0
+        speed[0] = -randint(speed[0]-1, speed[0]+1)
+
     if(position[1] > 64 or position[1] < 1):
         position[1] = 64
+        speed[1] = -randint(speed[1]-1, speed[1]+1)
+
+    if(position[1] < 0):
+        position[1] = 0
         speed[1] = -randint(speed[1]-1, speed[1]+1)
 
     display.pixel(position[0], position[1], 1)
